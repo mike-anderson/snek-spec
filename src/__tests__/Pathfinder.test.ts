347388fadbd79855486754befed12760c60d6dfa
Yves <yfreypons@gmail.com>
2020-02-28 15:23:57 -0800

    Add fixtures and folder structure to test dir (#21)

--- src/__tests__/Pathfinder.test.ts
+++ src/__tests__/Pathfinder.test.ts
@@ -1,41 +1,5 @@
-import { ISnake } from '../Types';
 import Pathfinder from '../Pathfinder';
-
-const us: ISnake = {
-  id: 'echosnek',
-  name: 'Echosnek5000',
-  health: 90,
-  body: [
-    { x: 6, y: 8 },
-    { x: 7, y: 8 },
-    { x: 8, y: 8 },
-  ],
-};
-
-const gameState = {
-  game: {
-    id: 'game-id-string',
-  },
-  turn: 4,
-  board: {
-    height: 15,
-    width: 15,
-    food: [{ x: 5, y: 5 }],
-    snakes: [
-      us,
-      {
-        id: 'enemy',
-        name: 'Nemesis',
-        health: 100,
-        body: [
-          { x: 1, y: 1 },
-          { x: 1, y: 2 },
-        ],
-      },
-    ],
-  },
-  you: us,
-};
+import { gameState, us } from './fixtures/Gamestate';
 
 const PF = new Pathfinder(gameState.board, gameState.board.snakes);
 
