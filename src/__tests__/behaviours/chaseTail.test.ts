590d1ebef2b8a57e4b3e6592489587c185130fc2
mbaart-echosec <59579294+mbaart-echosec@users.noreply.github.com>
2020-03-06 16:30:55 -0800

    Refactor function to eliminate initialization of new Pathfinder class (#32)
    * Refactor function to eliminate initialization of new Pathfinder class
    
    * Fix prettier problem
    
    * Update function description/params

--- src/__tests__/behaviours/chaseTail.test.ts
+++ src/__tests__/behaviours/chaseTail.test.ts
@@ -1,16 +1,19 @@
 import { ISnake, IBoard, Directions } from '../../Types';
 import { chaseTail } from '../../behaviours/chaseTail';
 import { gameState } from '../fixtures/Gamestate';
+import Pathfinder from '../../Pathfinder';
 
 describe('Chase tail behaviour', () => {
   test('should return a direction for the shortest path to our own tail', () => {
     // Arrange
     const board: IBoard = gameState.board;
+    const snakes: ISnake[] = gameState.board.snakes;
+    const PF: Pathfinder = new Pathfinder(board, snakes);
     const us: ISnake = gameState.board.snakes[0];
     const expectedDirection: string = Directions.LEFT;
 
     // Act
-    const actualDirection = chaseTail(board, us);
+    const actualDirection = chaseTail(PF, us);
 
     expect(actualDirection).toBe(expectedDirection);
   });
--- src/behaviours/chaseTail.ts
+++ src/behaviours/chaseTail.ts
@@ -1,15 +1,12 @@
-import { ISnake, IBoard, ICoordinate, Directions } from '../Types';
+import { ISnake, ICoordinate, Directions } from '../Types';
 import Pathfinder from '../Pathfinder';
 
 /**
- * @param {IBoard} board - the board state
+ * @param {Pathfinder} PF - Pathfinder class initialized with game state
  * @param {ISnake} us - our snake
  * @returns {Directions} returns the next direction
  */
-export const chaseTail = (board: IBoard, us: ISnake): Directions => {
-  const snakes: ISnake[] = board.snakes;
-  const PF = new Pathfinder(board, snakes);
-
+export const chaseTail = (PF: Pathfinder, us: ISnake): Directions => {
   const head: ICoordinate = us.body[0];
   const tail: ICoordinate = us.body[us.body.length - 1];
 
